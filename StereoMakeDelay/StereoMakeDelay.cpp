68105e3e99b25dcd57f73e4d8dcbcb23de457421
haru19970409 <haru19970409@gmail.com>
2020-05-05 20:10:42 +0900

    プロジェクト ファイルを追加します。

---
+++ StereoMakeDelay/StereoMakeDelay.cpp
@@ -0,0 +1,344 @@
+﻿// StereoMakeDelay.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+
+#define _USE_MATH_DEFINES
+#define ERROR_CHECK( ret )											\
+	if( FAILED( ret ) ){											\
+		std::stringstream ss;										\
+		ss << "failed " #ret " " << std::hex << ret << std::endl;	\
+		throw std::runtime_error( ss.str().c_str() );				\
+	}
+#define NUM_BUFFER 2
+#define NUM_SOURCE 2
+
+///////////StereoGenerate
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <al.h>
+#include <alc.h>
+#include <math.h>
+#include <conio.h>
+#include <efx.h>//20200505
+#include <efx-presets.h>//20200505
+
+#pragma comment(lib, "OpenAL32.lib")
+//fopenの警告を無視
+#pragma warning(disable:4996)
+
+/* Effect object functions */
+static LPALGENEFFECTS alGenEffects;
+static LPALDELETEEFFECTS alDeleteEffects;
+static LPALISEFFECT alIsEffect;
+static LPALEFFECTI alEffecti;
+static LPALEFFECTIV alEffectiv;
+static LPALEFFECTF alEffectf;
+static LPALEFFECTFV alEffectfv;
+static LPALGETEFFECTI alGetEffecti;
+static LPALGETEFFECTIV alGetEffectiv;
+static LPALGETEFFECTF alGetEffectf;
+static LPALGETEFFECTFV alGetEffectfv;
+
+
+////立体音響の生成///////
+class StereoGenerate {
+
+public:
+	//ファイルを開くときの変数
+	char type[4];
+	DWORD size, chunkSize;
+	short formatType, channels;
+	DWORD sampleRate, avgBytesPerSec;
+	short bytesPerSample, bitsPerSample;
+	unsigned long dataSize;
+
+	//1サンプルの長さ
+	unsigned long memory;
+	//どのくらい読み込むか
+	unsigned long length;
+	signed short* wav_data;
+
+	ALuint buffer;
+	ALuint sources[NUM_SOURCE];
+	ALfloat ListenerPos[3];
+
+
+	int SoundSet(const char* SoundName) {
+		FILE* fp;
+		//unsigned long memory = 0, length = 0;
+
+		fp = fopen(SoundName, "rb");
+		if (!fp) {
+			printf("ファイルを開けない\n");
+			return 1;
+		}
+
+		//Check that the WAVE file is OK
+		fread(type, sizeof(char), 4, fp);
+		if (type[0] != 'R' || type[1] != 'I' || type[2] != 'F' || type[3] != 'F') {
+			printf("not 'RIFF'\n");
+			return 1;
+		}
+		fread(&size, sizeof(DWORD), 1, fp);
+		fread(type, sizeof(char), 4, fp);
+		if (type[0] != 'W' || type[1] != 'A' || type[2] != 'V' || type[3] != 'E') {
+			printf("not 'WAVE'\n");
+			return 1;
+		}
+		fread(type, sizeof(char), 4, fp);
+		if (type[0] != 'f' || type[1] != 'm' || type[2] != 't' || type[3] != ' ') {
+			printf("not 'fmt '\n");
+			return 1;
+		}
+		//ここからchunkSizeバイト分がwavのパラメータ領域
+		fread(&chunkSize, sizeof(DWORD), 1, fp);
+		//基本的にpcm形式
+		fread(&formatType, sizeof(short), 1, fp);
+		//wavのチャンネル数
+		fread(&channels, sizeof(short), 1, fp);
+		//サンプリング周波数
+		fread(&sampleRate, sizeof(DWORD), 1, fp);
+		//1秒間の平均転送レート(=channels*sampleRate*bitsPerSample / 8)
+		fread(&avgBytesPerSec, sizeof(DWORD), 1, fp);
+		//各サンプル数のbyte数(例えば、16bit, 2channelsなら4)
+		fread(&bytesPerSample, sizeof(short), 1, fp);
+		//量子化ビット数　(16, 8)
+		fread(&bitsPerSample, sizeof(short), 1, fp);
+
+		//波形データ長
+		fread(&dataSize, sizeof(unsigned long), 1, fp);
+		//1サンプルの長さ
+		memory = bitsPerSample / 8;
+		//どのくらい読み込むか
+		length = dataSize / memory;
+		wav_data = new signed short[length];
+
+		/*for (int i = 0; i < 256; i++) {
+			fread(type, sizeof(char), 1, fp);
+			if (type[0] == 'd') {
+				fread(type, sizeof(char), 1, fp);
+				if (type[0] == 'a') {
+					fread(type, sizeof(char), 1, fp);
+					if (type[0] == 't') {
+						fread(type, sizeof(char), 1, fp);
+						if (type[0] == 'a') {
+							break;
+						}
+					}
+				}
+			}
+			if (i == 255) {
+				printf("データがありません。\n");
+				return 1;
+			}
+		}*/
+
+		//波形データ
+		fread(wav_data, memory, length, fp);
+		fclose(fp);
+		return 0;
+
+	}
+	int SoundPlay() {
+		////////////////////////////////////////////
+		const ALCchar* deviceList = alcGetString(nullptr, ALC_ALL_DEVICES_SPECIFIER);
+		printf(deviceList);
+		//デバイスを開く
+		ALCdevice* device = alcOpenDevice(nullptr);
+		//コンテキストを生成
+		ALCcontext* context = alcCreateContext(device, nullptr);
+		//生成したコンテキストを操作対象にする
+		alcMakeContextCurrent(context);
+
+		//CreateBufferObject & Create Source Object
+		alGenBuffers(1, &buffer); //曲データ１つにつきバッファ１つ．
+        // 3.source用意
+		alGenSources(NUM_SOURCE, sources); //空間に配置する数の分生成する.
+
+		ALuint frequency = sampleRate;
+
+		//ステレオだった場合モノラルに統合
+		if (channels == 2) {
+			for (int i = 0; i < length; i += 2) {
+				//平均値をとる
+				wav_data[i / 2] = wav_data[i] / 2 + wav_data[i + 1] / 2;
+			}
+		}
+		//長さもモノラルに直す
+		length = length / channels;
+		if (bitsPerSample == 8) {
+			printf("8bit\n");
+			return 0;
+		}
+
+		ALuint format = AL_FORMAT_MONO16;
+		// bufferによみこみ
+		for (int i = 0; i < NUM_BUFFER; i++) {
+			alBufferData(buffer, format, &wav_data[0], length * sizeof(signed short), sampleRate);
+		}
+
+		// source と buffer の接続
+		for (int i = 0; i < NUM_SOURCE; i++) {
+			alSourcei(sources[i], AL_BUFFER, buffer);
+		}
+
+		// 4.sourceのプロパティ設定
+		for (int i = 0; i < NUM_SOURCE; i++) {
+			alSourcei(sources[i], AL_LOOPING, AL_TRUE);   // 繰り返し
+			alSourcei(sources[i], AL_PITCH, 1.0f);      //
+			alSourcei(sources[i], AL_GAIN, 1.0f);     // 音量
+			//alSource3f(sources[i], AL_POSITION, 0, 0, 0); // 音源位置
+		}
+		/*alSource3f(sources[0], AL_POSITION, 0, 0, 0); // 音源位置
+		alSource3f(sources[1], AL_POSITION, -1, 0, 0); // 音源位置
+
+		alSourcei(sources[0], AL_SEC_OFFSET, 0.0);
+		alSourcei(sources[1], AL_SEC_OFFSET, 0.0);*/
+
+		/*float offset;
+
+		alGetSourcef(source, AL_SEC_OFFSET, &offset);
+
+		return offset;*/
+		// 7.再生
+		alSourcePlayv(NUM_SOURCE,sources);
+
+		for (int i = 0; i <= 360; i++) {
+			alSource3f(sources[0], AL_POSITION, cos(2 * M_PI * i / 360), 0.0, sin(2 * M_PI * i / 360));
+			alSource3f(sources[1], AL_POSITION, cos(2 * M_PI * i / 360), 0.0, sin(2 * M_PI * i / 360));
+			Sleep(30);
+
+		}
+		
+		//バッファに格納したので消してよい
+		delete[] wav_data;
+		printf("再生中\n");
+		printf("buffer = %u\n", buffer);
+		printf("source = %u\n", sources);
+		/////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////
+		/*alDeleteBuffers(1, &buffer);
+		alDeleteSources(1, &source);
+
+		//OpenALの後始末
+		//操作対象のコンテキストを解除
+		alcMakeContextCurrent(nullptr);
+		//コンテキストを破棄
+		alcDestroyContext(context);
+		//デバイスを閉じる
+		alcCloseDevice(device);*/
+
+		return 0;
+		//ここちゃんと、gestureとかでメモリ解放するように実装する
+	}
+
+	void UpdateListener(ALfloat Listener[3], float x, float y, float z) {
+		//リスナー(自分)を空間座標に配置
+		Listener[0] = x;
+		Listener[1] = y;
+		Listener[2] = z;
+	}
+};
+
+
+/* LoadEffect loads the given reverb properties into a new OpenAL effect
+ * object, and returns the new effect ID. */
+static ALuint LoadEffect(const EFXEAXREVERBPROPERTIES* reverb)
+{
+	ALuint effect = 0;
+	ALenum err;
+
+	/* Create the effect object and check if we can do EAX reverb. */
+	alGenEffects(1, &effect);
+	if (alGetEnumValue("AL_EFFECT_EAXREVERB") != 0)
+	{
+		printf("Using EAX Reverb\n");
+
+		/* EAX Reverb is available. Set the EAX effect type then load the
+		 * reverb properties. */
+		alEffecti(effect, AL_EFFECT_TYPE, AL_EFFECT_EAXREVERB);
+
+		alEffectf(effect, AL_EAXREVERB_DENSITY, reverb->flDensity);
+		alEffectf(effect, AL_EAXREVERB_DIFFUSION, reverb->flDiffusion);
+		alEffectf(effect, AL_EAXREVERB_GAIN, reverb->flGain);
+		alEffectf(effect, AL_EAXREVERB_GAINHF, reverb->flGainHF);
+		alEffectf(effect, AL_EAXREVERB_GAINLF, reverb->flGainLF);
+		alEffectf(effect, AL_EAXREVERB_DECAY_TIME, reverb->flDecayTime);
+		alEffectf(effect, AL_EAXREVERB_DECAY_HFRATIO, reverb->flDecayHFRatio);
+		alEffectf(effect, AL_EAXREVERB_DECAY_LFRATIO, reverb->flDecayLFRatio);
+		alEffectf(effect, AL_EAXREVERB_REFLECTIONS_GAIN, reverb->flReflectionsGain);
+		alEffectf(effect, AL_EAXREVERB_REFLECTIONS_DELAY, reverb->flReflectionsDelay);
+		alEffectfv(effect, AL_EAXREVERB_REFLECTIONS_PAN, reverb->flReflectionsPan);
+		alEffectf(effect, AL_EAXREVERB_LATE_REVERB_GAIN, reverb->flLateReverbGain);
+		alEffectf(effect, AL_EAXREVERB_LATE_REVERB_DELAY, reverb->flLateReverbDelay);
+		alEffectfv(effect, AL_EAXREVERB_LATE_REVERB_PAN, reverb->flLateReverbPan);
+		alEffectf(effect, AL_EAXREVERB_ECHO_TIME, reverb->flEchoTime);
+		alEffectf(effect, AL_EAXREVERB_ECHO_DEPTH, reverb->flEchoDepth);
+		alEffectf(effect, AL_EAXREVERB_MODULATION_TIME, reverb->flModulationTime);
+		alEffectf(effect, AL_EAXREVERB_MODULATION_DEPTH, reverb->flModulationDepth);
+		alEffectf(effect, AL_EAXREVERB_AIR_ABSORPTION_GAINHF, reverb->flAirAbsorptionGainHF);
+		alEffectf(effect, AL_EAXREVERB_HFREFERENCE, reverb->flHFReference);
+		alEffectf(effect, AL_EAXREVERB_LFREFERENCE, reverb->flLFReference);
+		alEffectf(effect, AL_EAXREVERB_ROOM_ROLLOFF_FACTOR, reverb->flRoomRolloffFactor);
+		alEffecti(effect, AL_EAXREVERB_DECAY_HFLIMIT, reverb->iDecayHFLimit);
+	}
+	else
+	{
+		printf("Using Standard Reverb\n");
+
+		/* No EAX Reverb. Set the standard reverb effect type then load the
+		 * available reverb properties. */
+		alEffecti(effect, AL_EFFECT_TYPE, AL_EFFECT_REVERB);
+
+		alEffectf(effect, AL_REVERB_DENSITY, reverb->flDensity);
+		alEffectf(effect, AL_REVERB_DIFFUSION, reverb->flDiffusion);
+		alEffectf(effect, AL_REVERB_GAIN, reverb->flGain);
+		alEffectf(effect, AL_REVERB_GAINHF, reverb->flGainHF);
+		alEffectf(effect, AL_REVERB_DECAY_TIME, reverb->flDecayTime);
+		alEffectf(effect, AL_REVERB_DECAY_HFRATIO, reverb->flDecayHFRatio);
+		alEffectf(effect, AL_REVERB_REFLECTIONS_GAIN, reverb->flReflectionsGain);
+		alEffectf(effect, AL_REVERB_REFLECTIONS_DELAY, reverb->flReflectionsDelay);
+		alEffectf(effect, AL_REVERB_LATE_REVERB_GAIN, reverb->flLateReverbGain);
+		alEffectf(effect, AL_REVERB_LATE_REVERB_DELAY, reverb->flLateReverbDelay);
+		alEffectf(effect, AL_REVERB_AIR_ABSORPTION_GAINHF, reverb->flAirAbsorptionGainHF);
+		alEffectf(effect, AL_REVERB_ROOM_ROLLOFF_FACTOR, reverb->flRoomRolloffFactor);
+		alEffecti(effect, AL_REVERB_DECAY_HFLIMIT, reverb->iDecayHFLimit);
+	}
+
+	/* Check if an error occured, and clean up if so. */
+	err = alGetError();
+	if (err != AL_NO_ERROR)
+	{
+		fprintf(stderr, "OpenAL error: %s\n", alGetString(err));
+		if (alIsEffect(effect))
+			alDeleteEffects(1, &effect);
+		return 0;
+	}
+
+	return effect;
+}
+
+
+
+int main()
+{
+	while(1) {
+		StereoGenerate Sound;
+		static bool IsFirst = true;
+		if (IsFirst && Sound.SoundSet("asano.wav") == 0) {
+			Sound.SoundPlay();
+			IsFirst = false;
+		}
+		if ('\r' == getch()) break;
+	}
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
