dcb45206ecc6e8eeadde103f430ce018106068a5
haru19970409 <haru19970409@gmail.com>
2020-05-05 22:59:43 +0900

    effect追加できるように

--- StereoMakeDelay/StereoMakeDelay.cpp
+++ StereoMakeDelay/StereoMakeDelay.cpp
@@ -11,6 +11,10 @@
 #define NUM_SOURCE 2
 
 ///////////StereoGenerate
+#include <assert.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <iostream>
 #include <windows.h>
 #include <stdio.h>
@@ -20,6 +24,10 @@
 #include <conio.h>
 #include <efx.h>//20200505
 #include <efx-presets.h>//20200505
+#include <alhelpers.h>//20200505
+#include <sndfile.h>//20200505
+
+
 
 #pragma comment(lib, "OpenAL32.lib")
 //fopenの警告を無視
@@ -38,6 +46,19 @@ static LPALGETEFFECTIV alGetEffectiv;
 static LPALGETEFFECTF alGetEffectf;
 static LPALGETEFFECTFV alGetEffectfv;
 
+
+/* Auxiliary Effect Slot object functions */
+static LPALGENAUXILIARYEFFECTSLOTS alGenAuxiliaryEffectSlots;
+static LPALDELETEAUXILIARYEFFECTSLOTS alDeleteAuxiliaryEffectSlots;
+static LPALISAUXILIARYEFFECTSLOT alIsAuxiliaryEffectSlot;
+static LPALAUXILIARYEFFECTSLOTI alAuxiliaryEffectSloti;
+static LPALAUXILIARYEFFECTSLOTIV alAuxiliaryEffectSlotiv;
+static LPALAUXILIARYEFFECTSLOTF alAuxiliaryEffectSlotf;
+static LPALAUXILIARYEFFECTSLOTFV alAuxiliaryEffectSlotfv;
+static LPALGETAUXILIARYEFFECTSLOTI alGetAuxiliaryEffectSloti;
+static LPALGETAUXILIARYEFFECTSLOTIV alGetAuxiliaryEffectSlotiv;
+static LPALGETAUXILIARYEFFECTSLOTF alGetAuxiliaryEffectSlotf;
+static LPALGETAUXILIARYEFFECTSLOTFV alGetAuxiliaryEffectSlotfv;
 
 ////立体音響の生成///////
 class StereoGenerate {
@@ -213,7 +234,7 @@ public:
 		delete[] wav_data;
 		printf("再生中\n");
 		printf("buffer = %u\n", buffer);
-		printf("source = %u\n", sources);
+		//printf("source = %u\n", sources);
 		/////////////////////////////////////////////////////////
 		///////////////////////////////////////////////////////////
 		/*alDeleteBuffers(1, &buffer);
@@ -317,12 +338,207 @@ static ALuint LoadEffect(const EFXEAXREVERBPROPERTIES* reverb)
 	return effect;
 }
 
-
-
-int main()
+/* LoadBuffer loads the named audio file into an OpenAL buffer object, and
+ * returns the new buffer ID.
+ */
+static ALuint LoadSound(const char* filename)
 {
+	ALenum err, format;
+	ALuint buffer;
+	SNDFILE* sndfile;
+	SF_INFO sfinfo;
+	short* membuf;
+	sf_count_t num_frames;
+	ALsizei num_bytes;
+
+	/* Open the audio file and check that it's usable. */
+	sndfile = sf_open(filename, SFM_READ, &sfinfo);
+	if (!sndfile)
+	{
+		fprintf(stderr, "Could not open audio in %s: %s\n", filename, sf_strerror(sndfile));
+		return 0;
+	}
+	if (sfinfo.frames < 1 || sfinfo.frames >(sf_count_t)(INT_MAX / sizeof(short)) / sfinfo.channels)
+	{
+		fprintf(stderr, "Bad sample count in %s (%" PRId64 ")\n", filename, sfinfo.frames);
+		sf_close(sndfile);
+		return 0;
+	}
+
+	/* Get the sound format, and figure out the OpenAL format */
+	if (sfinfo.channels == 1)
+		format = AL_FORMAT_MONO16;
+	else if (sfinfo.channels == 2)
+		format = AL_FORMAT_STEREO16;
+	else
+	{
+		fprintf(stderr, "Unsupported channel count: %d\n", sfinfo.channels);
+		sf_close(sndfile);
+		return 0;
+	}
+
+	/* Decode the whole audio file to a buffer. */
+	membuf = malloc((size_t)(sfinfo.frames * sfinfo.channels) * sizeof(short));
+
+	num_frames = sf_readf_short(sndfile, membuf, sfinfo.frames);
+	if (num_frames < 1)
+	{
+		free(membuf);
+		sf_close(sndfile);
+		fprintf(stderr, "Failed to read samples in %s (%" PRId64 ")\n", filename, num_frames);
+		return 0;
+	}
+	num_bytes = (ALsizei)(num_frames * sfinfo.channels) * (ALsizei)sizeof(short);
+
+	/* Buffer the audio data into a new buffer object, then free the data and
+	 * close the file.
+	 */
+	buffer = 0;
+	alGenBuffers(1, &buffer);
+	alBufferData(buffer, format, membuf, num_bytes, sfinfo.samplerate);
+
+	free(membuf);
+	sf_close(sndfile);
+
+	/* Check if an error occured, and clean up if so. */
+	err = alGetError();
+	if (err != AL_NO_ERROR)
+	{
+		fprintf(stderr, "OpenAL Error: %s\n", alGetString(err));
+		if (buffer && alIsBuffer(buffer))
+			alDeleteBuffers(1, &buffer);
+		return 0;
+	}
+
+	return buffer;
+}
+
+int main(int argc, char** argv)
+{
+	EFXEAXREVERBPROPERTIES reverb = EFX_REVERB_PRESET_GENERIC;
+	ALuint source, buffer, effect, slot;
+	ALenum state;
+
+	/* Print out usage if no arguments were specified */
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s [-device <name] <filename>\n", argv[0]);
+		return 1;
+	}
+
+	/* Initialize OpenAL, and check for EFX support. */
+	argv++; argc--;
+	if (InitAL(&argv, &argc) != 0)
+		return 1;
+
+	if (!alcIsExtensionPresent(alcGetContextsDevice(alcGetCurrentContext()), "ALC_EXT_EFX"))
+	{
+		fprintf(stderr, "Error: EFX not supported\n");
+		CloseAL();
+		return 1;
+	}
+
+	/* Define a macro to help load the function pointers. */
+#define LOAD_PROC(T, x)  ((x) = (T)alGetProcAddress(#x))
+	LOAD_PROC(LPALGENEFFECTS, alGenEffects);
+	LOAD_PROC(LPALDELETEEFFECTS, alDeleteEffects);
+	LOAD_PROC(LPALISEFFECT, alIsEffect);
+	LOAD_PROC(LPALEFFECTI, alEffecti);
+	LOAD_PROC(LPALEFFECTIV, alEffectiv);
+	LOAD_PROC(LPALEFFECTF, alEffectf);
+	LOAD_PROC(LPALEFFECTFV, alEffectfv);
+	LOAD_PROC(LPALGETEFFECTI, alGetEffecti);
+	LOAD_PROC(LPALGETEFFECTIV, alGetEffectiv);
+	LOAD_PROC(LPALGETEFFECTF, alGetEffectf);
+	LOAD_PROC(LPALGETEFFECTFV, alGetEffectfv);
+
+	LOAD_PROC(LPALGENAUXILIARYEFFECTSLOTS, alGenAuxiliaryEffectSlots);
+	LOAD_PROC(LPALDELETEAUXILIARYEFFECTSLOTS, alDeleteAuxiliaryEffectSlots);
+	LOAD_PROC(LPALISAUXILIARYEFFECTSLOT, alIsAuxiliaryEffectSlot);
+	LOAD_PROC(LPALAUXILIARYEFFECTSLOTI, alAuxiliaryEffectSloti);
+	LOAD_PROC(LPALAUXILIARYEFFECTSLOTIV, alAuxiliaryEffectSlotiv);
+	LOAD_PROC(LPALAUXILIARYEFFECTSLOTF, alAuxiliaryEffectSlotf);
+	LOAD_PROC(LPALAUXILIARYEFFECTSLOTFV, alAuxiliaryEffectSlotfv);
+	LOAD_PROC(LPALGETAUXILIARYEFFECTSLOTI, alGetAuxiliaryEffectSloti);
+	LOAD_PROC(LPALGETAUXILIARYEFFECTSLOTIV, alGetAuxiliaryEffectSlotiv);
+	LOAD_PROC(LPALGETAUXILIARYEFFECTSLOTF, alGetAuxiliaryEffectSlotf);
+	LOAD_PROC(LPALGETAUXILIARYEFFECTSLOTFV, alGetAuxiliaryEffectSlotfv);
+#undef LOAD_PROC
+
+	/* Load the sound into a buffer. */
+	buffer = LoadSound(argv[0]);
+	if (!buffer)
+	{
+		CloseAL();
+		return 1;
+	}
+
+	/* Load the reverb into an effect. */
+	effect = LoadEffect(&reverb);
+	if (!effect)
+	{
+		alDeleteBuffers(1, &buffer);
+		CloseAL();
+		return 1;
+	}
+
+	/* Create the effect slot object. This is what "plays" an effect on sources
+	 * that connect to it. */
+	slot = 0;
+	alGenAuxiliaryEffectSlots(1, &slot);
+
+	/* Tell the effect slot to use the loaded effect object. Note that the this
+	 * effectively copies the effect properties. You can modify or delete the
+	 * effect object afterward without affecting the effect slot.
+	 */
+	alAuxiliaryEffectSloti(slot, AL_EFFECTSLOT_EFFECT, (ALint)effect);
+	assert(alGetError() == AL_NO_ERROR && "Failed to set effect slot");
+
+	/* Create the source to play the sound with. */
+	source = 0;
+	alGenSources(1, &source);
+	alSourcei(source, AL_BUFFER, (ALint)buffer);
+
+	/* Connect the source to the effect slot. This tells the source to use the
+	 * effect slot 'slot', on send #0 with the AL_FILTER_NULL filter object.
+	 */
+	alSource3i(source, AL_AUXILIARY_SEND_FILTER, (ALint)slot, 0, AL_FILTER_NULL);
+	assert(alGetError() == AL_NO_ERROR && "Failed to setup sound source");
+
+	/* Play the sound until it finishes. */
+	alSourcePlay(source);
+	do {
+		al_nssleep(10000000);
+		alGetSourcei(source, AL_SOURCE_STATE, &state);
+	} while (alGetError() == AL_NO_ERROR && state == AL_PLAYING);
+
+	/* All done. Delete resources, and close down OpenAL. */
+	alDeleteSources(1, &source);
+	alDeleteAuxiliaryEffectSlots(1, &slot);
+	alDeleteEffects(1, &effect);
+	alDeleteBuffers(1, &buffer);
+
+	CloseAL();
+
+	return 0;
+}
+
+/*int main(int argc, char** argv)
+{
+	EFXEAXREVERBPROPERTIES reverb = EFX_REVERB_PRESET_GENERIC;
+	// Initialize OpenAL, and check for EFX support. 
+	argv++; argc--;
+	if (InitAL(&argv, &argc) != 0)
+		return 1;
+
+	if (!alcIsExtensionPresent(alcGetContextsDevice(alcGetCurrentContext()), "ALC_EXT_EFX"))
+	{
+		fprintf(stderr, "Error: EFX not supported\n");
+		CloseAL();
+		return 1;
+	}
 	while(1) {
-		StereoGenerate Sound;
+		//StereoGenerate Sound;
 		static bool IsFirst = true;
 		if (IsFirst && Sound.SoundSet("asano.wav") == 0) {
 			Sound.SoundPlay();
@@ -330,7 +546,7 @@ int main()
 		}
 		if ('\r' == getch()) break;
 	}
-}
+}*/
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
