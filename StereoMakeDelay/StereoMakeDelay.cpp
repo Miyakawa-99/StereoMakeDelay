bd19aeb59480a93bbcefd3e4c05a0ccacdf6bdc1
haru19970409 <haru19970409@gmail.com>
2020-07-19 22:45:10 +0900

    streaming

--- StereoMakeDelay/StereoMakeDelay.cpp
+++ StereoMakeDelay/StereoMakeDelay.cpp
@@ -1,69 +1,80 @@
-﻿#define _USE_MATH_DEFINES
-#include < al.h >
-#include < alc.h >
-#include < array >
-#include < cmath > 
-#include < limits > 
-#include < vector > 
+﻿#include <al.h>
+#include <alc.h>
+#include <iostream>
 
+#define SAMPLINGRATE 44100 
+#define BUFFER_SIZE 4410
 
-int main() { 
-	// OpenAL の 初期化 
-	ALCdevice* device = alcOpenDevice(nullptr); 
-	ALCcontext* context = alcCreateContext(device, nullptr); 
-	alcMakeContextCurrent(context); 
-	// バッファ の 生成 
-	const int queue_buffer_num = 4; 
-	std:: array < ALuint, queue_buffer_num > buffer_id; 
-	alGenBuffers( buffer_id.size(), &buffer_id[ 0]); 
-	for (size_t i = 0; i < buffer_id.size(); ++ i) { 
-		// 一 秒 ぶん の サイン 波 を 生成 
-		const size_t pcm_freq = 44100; 
-		const float key_freq = 440.0 * (i + 1); 
-		std:: array < ALshort, pcm_freq > pcm_data; 
-		for (size_t h = 0; h < pcm_data.size(); ++ h) { 
-			pcm_data[h] = std:: sin( key_freq * M_PI * 2.0 * h / pcm_freq) * 
-				std:: numeric_limits < ALshort >:: max(); 
-		} 
-		// サイン 波 を バッファ に コピー 
-		alBufferData( buffer_id[i], AL_FORMAT_MONO16, &pcm_data[0], 
-			pcm_data.size() * sizeof(ALshort), pcm_freq);
-	} 
-	// ソース の 生成 
-	ALuint source_id; alGenSources(1, &source_id); 
-	// バッファ を キューイング
-	alSourceQueueBuffers(source_id, buffer_id.size(), &buffer_id[ 0]); 
-	// ソース の 再生 開始 
-	alSourcePlay(source_id); 
-	int loop_count = 12; 
-	while (loop_count != 0) { 
-		// 再生 の 終わっ た バッファ が ある か?? 
-		// 再生 の 終わっ た バッファ 数 を processed に 格納 する 
-		int processed; 
-		alGetSourcei(source_id, AL_BUFFERS_PROCESSED, &processed); 
-		if (processed > 0) { 
-			// 再生 の 終わっ た バッファ を 再利用 
-			std:: vector < ALuint > unqueue_buffer_id(processed); 
-			alSourceUnqueueBuffers(source_id, processed, &unqueue_buffer_id[0]); 
-			// 再 キューイング
-			alSourceQueueBuffers(source_id, processed, &unqueue_buffer_id[0]); 
-			--loop_count;
+using namespace std;
+int main(void) {
+	//マイクセットアップ
+	ALCdevice* mic = alcCaptureOpenDevice(NULL, SAMPLINGRATE, AL_FORMAT_MONO16, BUFFER_SIZE);
+	//スピーカーセットアップ
+	ALCdevice* speaker = alcOpenDevice(NULL);
+	//いつもの
+	ALCcontext* context = alcCreateContext(speaker, NULL);
+	alcMakeContextCurrent(context);
+
+	//バッファ(保存領域)とソース(音源)を宣言
+	//ストリーミング用にバッファを二つ
+	ALuint buffer[2];
+	ALuint source;
+	//それを生成
+	alGenBuffers(2, &buffer[0]);
+	alGenSources(1, &source);
+
+	//マイクから録音した音を一旦入れておく
+	ALshort* store = new ALshort[BUFFER_SIZE];
+
+	//再生状態を監視するための準備
+	alBufferData(buffer[0], AL_FORMAT_MONO16, &store[0], 0, SAMPLINGRATE);
+	alSourceQueueBuffers(source, 1, &buffer[0]);
+	alSourcePlay(source);
+
+	//録音開始
+	alcCaptureStart(mic);
+	cout << "録音＆再生中・・・" << endl;
+
+	//録音と再生を制御
+	int a = 0, count = 100;
+	ALint sample, state;
+	while (1) {
+		//sourceが再生中か確認
+		alGetSourcei(source, AL_BUFFERS_PROCESSED, &state);
+		//録音可能なバッファ長を取得
+		alcGetIntegerv(mic, ALC_CAPTURE_SAMPLES, sizeof(sample), &sample);
+		//再生が終わり、録音が可能なとき
+		if (sample > 0 && state == 1) {
+			//録音してstoreに格納
+			alcCaptureSamples(mic, (ALCvoid*)&store[0], sample);
+			//再生バッファをソースから外す
+			alSourceUnqueueBuffers(source, 1, &buffer[a]);
+			//待機バッファをソースに適用
+			alSourceQueueBuffers(source, 1, &buffer[a ^ 1]);
+			//再生
+			alSourcePlay(source);
+			//待機バッファに録音した音を入れる
+			alBufferData(buffer[a], AL_FORMAT_MONO16, &store[0], sample * sizeof(unsigned short), SAMPLINGRATE);
+			//ここでバッファの切り替え
+			a = a ^ 1;
+			count--;
 		}
-	} 
+		if (count <= 0) {
+			break;
+		}
+	}
+	//マイク停止
+	alcCaptureStop(mic);
 
-	// ソース の 破棄 
-	alDeleteSources(1, &source_id); 
-	// バッファ の 破棄 
-	alDeleteBuffers(buffer_id.size(), &buffer_id[0]); 
-	// OpenAL の 後始末 
-	alcMakeContextCurrent(nullptr); 
-	alcDestroyContext(context); 
-	alcCloseDevice(device); 
+	//バッファ・ソースの後始末
+	alDeleteBuffers(2, &buffer[0]);
+	alDeleteSources(1, &source);
+
+	//OpenALの後始末
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(context);
+	//マイクを閉じる
+	alcCloseDevice(mic);
+	//スピーカを閉じる
+	alcCloseDevice(speaker);
 }
-
-
-
-
-
-
-
